d5765b27cf429ec928924d10cd62dd56050be347
seifessam60 <seifessam60@gmail>
2025-06-21 20:36:46 +0300

    Authentication

---
+++ mobile/app/components/SafeScreen.jsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+const SafeScreen = ({ children }) => {
+  const insets = useSafeAreaInsets();
+  return <View style={{ paddingTop: insets.top, flex: 1 }}>{children}</View>;
+};
+
+export default SafeScreen;
